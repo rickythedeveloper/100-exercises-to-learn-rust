983ad611ba11a0be58405e98502af9d5fb269ef4
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2025-01-02 22:10:37 +0900

    4.14

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,86 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Debug)]
+pub struct SaturatingU16 {
+	value: u16
+}
+
+impl Clone for SaturatingU16 {
+	fn clone(&self) -> Self {
+			SaturatingU16 { value: self.value }
+	}
+}
+
+impl Copy for SaturatingU16 {}
+
+impl PartialEq for SaturatingU16 {
+	fn eq(&self, other: &Self) -> bool {
+			self.value == other.value
+	}
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+	fn eq(&self, other: &u16) -> bool {
+			self.value == *other
+	}
+}
+
+impl Add for SaturatingU16 {
+	type Output = SaturatingU16;
+
+	fn add(self, rhs: Self) -> Self::Output {
+			SaturatingU16 { value: self.value.saturating_add(rhs.value) }
+	}
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+	type Output = SaturatingU16;
+
+	fn add(self, rhs: &SaturatingU16) -> Self::Output {
+			SaturatingU16 { value: self.value.saturating_add(rhs.value) }
+	}
+}
+
+impl Add<u16> for SaturatingU16 {
+	type Output = SaturatingU16;
+
+	fn add(self, rhs: u16) -> Self::Output {
+			SaturatingU16 { value: self.value.saturating_add(rhs)}
+	}
+}
+
+impl Add<&u16> for SaturatingU16 {
+	type Output = SaturatingU16;
+
+	fn add(self, rhs: &u16) -> Self::Output {
+			SaturatingU16 { value: self.value.saturating_add(*rhs)}
+	}
+}
+
+impl From<u16> for SaturatingU16 {
+	fn from(value: u16) -> Self {
+			SaturatingU16 { value: value.into() }
+	}
+}
+
+impl From<&u16> for SaturatingU16 {
+	fn from(value: &u16) -> Self {
+			SaturatingU16 { value: (*value).into() }
+	}
+}
+
+impl From<u8> for SaturatingU16 {
+	fn from(value: u8) -> Self {
+			SaturatingU16 { value: value.into() }
+	}
+}
+
+impl From<&u8> for SaturatingU16 {
+	fn from(value: &u8) -> Self {
+			SaturatingU16 { value: (*value).into() }
+	}
+}
